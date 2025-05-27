138acb36bced35022c96528212caf44bb405496b
muniz-gabriel <muniz.gabriel@aluno.ifsp.edu.br>
2025-05-27 14:38:53 -0300

    CRUD do Usuário✅

--- src/controllers/Usuario.Controller.js
+++ src/controllers/Usuario.Controller.js
@@ -19,6 +19,50 @@ class UsuarioController {
             return res.status(500).json({ error: 'Erro ao listar usuário' })
         }
     }
+
+    async show(req,res) {
+        const { email } = req.params
+        try {
+            const usuario = await Usuario.findOne({ where: { email} })
+            if (!usuario) {
+                return res.status(404).json({ error: 'Usuário não encontrado'})
+            }
+            return res.status(200).json(usuario)
+        } catch (error) {
+            return res.status(500).json({ error: 'Erro ao buscar usuário' })
+        }
+    }
+
+    async update(req, res) {
+        const { email } = req.params
+        const { nome, senha } = req.body
+        try {
+            const usuario = await Usuario.findOne({ where: { email } })
+            if (!usuario) {
+                return res.status(404).json({ error: 'Usuário não encontrado' })
+            }
+            usuario.nome = nome || usuario.nome
+            usuario.senha = senha || usuario.senha
+            await usuario.save()
+            return res.status(200).json(usuario)
+        } catch (error) {
+            return res.status(500).json({ error: 'Erro ao atualizar usuário' })
+        }
+    }
+
+    async delete(req, res) {
+        const { id } = req.params
+        try {
+            const usuario = await Usuario.findByPk(id)
+            if (!usuario) {
+                return res.status(404).json({ error: 'Usuário não encontrado' })
+            }
+            await usuario.destroy()
+            return res.status(204).send()
+        } catch (error) {
+            return res.status(500).json({ error: 'Erro ao deletar usuário' })
+        }
+    }
 }
 
 export default new UsuarioController()
